6fc170ffa77b98116de212c94e6da7d04d679b4a
Leon Yao <leon.yao117@gmail.com>
2023-12-03 13:32:18 +0000

    day3

---
+++ 2023/day03/part1.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def solution(input):
+    grid = np.array([list(line) for line in input])
+
+    def getPart(row, l, r) -> int:
+        """
+        grid[row][l:r+1] is the word
+        """
+        if l == -1 or r == -1:
+            return 0
+        tocheck = []
+        for i in range(l - 1, r + 2):
+            tocheck.append((row - 1, i))  # top row
+            tocheck.append((row + 1, i))  # bottom row
+        # left and right
+        tocheck.append((row, l - 1))
+        tocheck.append((row, r + 1))
+
+        for j, i in tocheck:
+            if j < 0 or j >= len(grid) or i < 0 or i >= len(grid[0]):
+                continue
+            if grid[j][i] != ".":
+                return int("".join(grid[row][l : r + 1]))
+
+        return 0
+
+    out = 0
+    for j, row in enumerate(grid):
+        left, right = -1, -1
+        for i, letter in enumerate(row):
+            if letter.isdigit():
+                if left == -1:
+                    left = i
+                right = i
+            else:
+                if left != -1:
+                    out += getPart(j, left, right)
+                    left, right = -1, -1
+        # edge case for last number
+        out += getPart(j, left, right)
+    return out
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        lines = [l.strip() for l in f.readlines()]
+
+    print(solution(lines))  # 528898 too high
