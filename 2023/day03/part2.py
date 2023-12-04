01aada49a504f541098a7e1b12bc9c061fc8ba3b
Leon Yao <leon.yao117@gmail.com>
2023-12-04 13:06:52 +0000

    clean code with regex

--- 2023/day03/part2.py
+++ 2023/day03/part2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 
 def solution(input):
@@ -7,45 +8,34 @@ def solution(input):
 
     def getGear(row, l, r) -> None:
         """
-        grid[row][l:r+1] is the word
+        grid[row][l:r] is the word
         """
         if l == -1 or r == -1:
             return
         tocheck = []
-        for i in range(l - 1, r + 2):
+        for i in range(l - 1, r + 1):
             tocheck.append((row - 1, i))  # top row
             tocheck.append((row + 1, i))  # bottom row
         # left and right
         tocheck.append((row, l - 1))
-        tocheck.append((row, r + 1))
+        tocheck.append((row, r))
 
         for j, i in tocheck:
             if j < 0 or j >= len(grid) or i < 0 or i >= len(grid[0]):
                 continue
             if grid[j][i] == "*":
-                gears[j][i].append(int("".join(grid[row][l : r + 1])))
+                gears[j][i].append(int("".join(grid[row][l:r])))
 
         return
 
-    out = 0
-    for j, row in enumerate(grid):
+    for i, row in enumerate(grid):
         left, right = -1, -1
-        for i, letter in enumerate(row):
-            if letter.isdigit():
-                if left == -1:
-                    left = i
-                right = i
-            else:
-                if left != -1:
-                    getGear(j, left, right)
-                    left, right = -1, -1
-        # edge case for last number
-        getGear(j, left, right)
-    
+        for match in re.finditer("[0-9]+", "".join(row)):
+            left, right = match.start(), match.end()
+            getGear(i, left, right)
+    out = 0
     for row in gears:
-        for gear in row:
-            if len(gear) == 2:
-                out += gear[0] * gear[1]
+        out += sum([g[0] * g[1] for g in row if len(g) == 2])
     return out
 
 
@@ -53,4 +43,4 @@ if __name__ == "__main__":
     with open("input.txt", "r") as f:
         lines = [l.strip() for l in f.readlines()]
 
-    print(solution(lines))  # 528898 too high
+    print(solution(lines))  # 84399773
