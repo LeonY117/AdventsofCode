3fc07be3df329573d7c293c95e16a3eb4954273a
Leon Yao <leon.yao117@gmail.com>
2023-12-05 13:37:26 +0000

    slightly refactor solution

--- 2023/day05/part2.py
+++ 2023/day05/part2.py
@@ -3,29 +3,29 @@ def solution(input):
     curr = {}
     for i in range(0, len(seed), 2):
         s, e = seed[i], seed[i] + seed[i + 1]
-        # each key is a tuple (start, end),
-        # value is the intervals that it maps to, default to itself
+        # key: tuple of source (s, e),
+        # value: list of target intervals (l, r), default to itself
         curr[(s, e)] = [(s, e)]
 
     for i in range(1, len(input)):
         line = input[i]
-        if not line[0].isdigit():
+        if not line[0].isdecimal():
             # make the values the new keys
             curr = {v: [v] for vs in curr.values() for v in vs}
-        else:
-            v, k, length = [int(n) for n in line.split(" ")]
-            for s, e in curr.keys():
-                l = max(s, k)
-                r = min(e, k + length)
-                # only updates if there's overlap interval [l, r]
-                if r > l:  # has to be strictly greater
-                    # overwrites the default self-mapping
-                    curr[(s, e)] = [(v + (l - k), v + (r - k))]
-                    # map non-overlapping region(s) to self
-                    if s < l:
-                        curr[(s, e)].append((s, l))
-                    if r < e:
-                        curr[(s, e)].append((r, e))
+            continue
+        v, k, length = [int(n) for n in line.split(" ")]
+        for s, e in curr.keys():
+            l = max(s, k)
+            r = min(e, k + length)
+            # only updates if there's overlap interval [l, r]
+            if r > l:  # has to be strictly greater
+                # overwrites the default self-mapping
+                curr[(s, e)] = [(v + (l - k), v + (r - k))]
+                # map non-overlapping region(s) to self
+                if s < l:
+                    curr[(s, e)].append((s, l))
+                if r < e:
+                    curr[(s, e)].append((r, e))
     out = min(min(v[0] for v in vs) for vs in curr.values())
     return out
 
