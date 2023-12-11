f8afeacbf5b3a06e6e0e562374fec741f7017dc0
Leon Yao <leon.yao117@gmail.com>
2023-12-11 21:45:40 +0000

    refactor part 1

--- 2023/day10/part1.py
+++ 2023/day10/part1.py
@@ -1,12 +1,16 @@
+from utils.utils import add, isWithinBounds
+
+
 def solution(input):
+    # storing all coordinates as [x, y]
     grid = [[cell for cell in line] for line in input]
     visited = [[False for _ in line] for line in input]
     # we need to BFS all the cells and get shortest path
     # each iteration we increase step count by 1
     # the increment for each new cell SHOULD be done synchronously
 
-    # the mapping from letter to [x, y] <-> [x, y]
-    left, right, down, up = (-1, 0), (1, 0), (0, 1), (0, -1)
+    # the mapping from letter to the two positions that they connect to
+    left, right, up, down = (-1, 0), (1, 0), (0, -1), (0, 1)
     lookup = {
         "|": (down, up),
         "-": (left, right),
@@ -14,67 +18,62 @@ def solution(input):
         "J": (up, left),
         "7": (left, down),
         "F": (down, right),
-        ".": [(0, 0), (0, 0)],
     }
 
-    def add(coord1, coord2):
-        return (coord1[0] + coord2[0], coord1[1] + coord2[1])
-
-    def isWithinBounds(coord):
-        return 0 <= coord[0] < len(grid[0]) and 0 <= coord[1] < len(grid)
-
-    def get_start_cell_neighbors(x, y):
+    def get_valid_neighbors(coord):
+        # returns the two valid neighbors that connect to x, y
         out = []
-        for dir in [left, right, down, up]:
-            neighbor = (x + dir[0], y + dir[1])
-            if not isWithinBounds(neighbor):
+        for direction in [left, right, down, up]:
+            # neighbor coordinate is [x, y] + direction
+            nb = add(coord, direction)
+            if not isWithinBounds(nb, grid):
                 continue
-            letter = grid[neighbor[1]][neighbor[0]]
-            connections = lookup[letter]
-            for i in range(2):
-                source = add(connections[i], neighbor)
-                # dest = add(invert(connections[(i + 1) % 2]), neighbor)
-                if source == (x, y):
-                    out.append(neighbor)
+            letter = grid[nb[1]][nb[0]]
+            connections = lookup.get(letter, [(0, 0)])
+            for connection in connections:
+                source = add(connection, nb)
+                # checks to see if the potential neighbor points back at the source
+                if source == coord:
+                    out.append(nb)
         return out
 
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == "S":
-                stack = get_start_cell_neighbors(x, y)
-                visited[y][x] = True
-                break
-        else:
-            continue
-        break
+    def find_start():
+        for y, row in enumerate(grid):
+            for x, cell in enumerate(row):
+                if cell == "S":
+                    return (x, y)
 
-    def bfs(x, y):
+    def bfs(coord):
         """
         appends the connected cell to stack if it's not visited,
-        there will always be one redundant check
+        (there will always be one redundant check)
         """
-        connections = lookup[grid[y][x]]
-        for i in range(2):
-            dest = add(connections[i], (x, y))
-            # dest = add(invert(connections[(i + 1) % 2]), (x, y))
-            if isWithinBounds(dest) and not visited[dest[1]][dest[0]]:
-                return [dest]
+        x, y = coord
+        connections = lookup.get(grid[y][x], [(0, 0)])
+        for connection in connections:
+            cell = add(connection, coord)
+            if isWithinBounds(cell, grid) and not visited[cell[1]][cell[0]]:
+                return [cell]
         return []
 
-    # BFS
+    start = find_start()
+    stack = get_valid_neighbors(start)
+    visited[start[1]][start[0]] = True
+    # BFS: we keep a stack with 2 items, and iterate to the next step when both are visited
     next = []
     steps = 0
+
     while stack:
         x, y = stack.pop(0)
-        # print(grid[y][x])
         if not visited[y][x]:
-            visited[y][x] = True
-            next.extend(bfs(x, y))
+            next.extend(bfs((x, y)))
 
         if not stack:
             steps += 1
             stack = next.copy()
             next = []
+
+        visited[y][x] = True
 
     return steps
 
