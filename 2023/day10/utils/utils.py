033dc519e0611a25f008b4755c9bea79c9b71790
Leon Yao <leon.yao117@gmail.com>
2023-12-11 21:45:35 +0000

    refactor utility funcs

---
+++ 2023/day10/utils/utils.py
@@ -0,0 +1,22 @@
+def invert(coord):
+    return (-coord[0], -coord[1])
+
+
+def add(coord1, coord2):
+    """adds two tuples element wise"""
+    return (coord1[0] + coord2[0], coord1[1] + coord2[1])
+
+
+def isWithinBounds(coord, grid):
+    """checks if a coordinate is within the grid"""
+    return 0 <= coord[0] < len(grid[0]) and 0 <= coord[1] < len(grid)
+
+
+def isOnBound(coord, grid):
+    """checks if a coordinate is on the boundary of the grid"""
+    return (
+        coord[0] == 0
+        or coord[0] == len(grid[0]) - 1
+        or coord[1] == 0
+        or coord[1] == len(grid) - 1
+    )
