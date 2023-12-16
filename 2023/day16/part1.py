8474726ffde6a3d41f87e054e24318dac1a118b3
Leon Yao <leon.yao117@gmail.com>
2023-12-16 22:45:01 +0000

    create placeholder files

---
+++ 2023/day16/part1.py
@@ -0,0 +1,8 @@
+def solution(input):
+    pass
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        lines = [l.strip() for l in f.readlines()]
+    
+    print(solution(lines))
