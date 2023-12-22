7f6b16d145299b046f5f4eb3986ab9360d7af361
Leon Yao <leon.yao117@gmail.com>
2023-12-22 13:38:46 +0000

    fix typo

--- 2023/aoc_today.sh
+++ 2023/aoc_today.sh
@@ -39,7 +39,7 @@ def solution(inp):
     pass
 
 if __name__ == "__main__":
-    with open("inp.txt", "r") as f:
+    with open("input.txt", "r") as f:
         lines = [l.strip() for l in f.readlines()]
     
     print(solution(lines))
