bc1cfe6119d89bbcacebd216855b3f793b8ba622
Leon Yao <leon.yao117@gmail.com>
2024-12-16 22:50:13 +0000

    add day 9 solutions

--- 2024/day09/part1.py
+++ 2024/day09/part1.py
@@ -1,17 +1,63 @@
 def solution(inp):
+    def tally_points(count, curr_idx, v):
+        indices = list(range(curr_idx, curr_idx + count))
+
+        return sum([v * i for i in indices])
+
     total = 0
-    counts = [inp[0]]
-    spaces = []
-    # one pass so that we know the value of the counts
-    for i in range(0, len(inp)-1, 2):
-        count, space = inp[i], inp[i+1]
-        counts.append(count)
-        spaces.append(space)
-    
-    print(counts, spaces)
+    nums = [int(n) for n in inp]
+
+    # pointers
+    l, r = 0, len(nums) - 1
+    # values
+    a, b = 0, (len(nums) - 1) // 2
+    # current index.
+    i = 0
+
+    while l <= r:
+        if l == r:
+            if l % 2 == 0:
+                total += tally_points(nums[l], i, a)
+            # If it's space we skip.
+            break
+
+        # Right side is space: always move on.
+        if r % 2 == 1:
+            b -= 1
+            r -= 1
+            continue
+
+        # Left side is number.
+        if l % 2 == 0:
+            total += tally_points(nums[l], i, a)
+            i += nums[l]
+            l += 1
+            a += 1
+        # Left side is space.
+        else:
+            # add b
+            spaces = nums[l]
+            # underfit
+            if nums[r] <= spaces:
+                total += tally_points(nums[r], i, b)
+                nums[l] -= nums[r]
+                i += nums[r]
+                r -= 1
+            # overfits
+            elif nums[r] > spaces:
+                total += tally_points(spaces, i, b)
+                i += spaces
+                nums[r] -= spaces
+                l += 1
+            # fills exactly
+            elif nums[r] == spaces:
+                l += 1
+
+    return total
+
 
 if __name__ == "__main__":
-    with open("test_input.txt", "r") as f:
+    with open("input.txt", "r") as f:
         line = f.readlines()[0].strip()
-    
-    print(solution(line))
+
+    print(solution(line))  # 6385338159127
--- 2024/day09/part2.py
+++ 2024/day09/part2.py
@@ -1,8 +1,44 @@
 def solution(inp):
-    pass
+    def tally_points(count, curr_idx, v):
+        indices = list(range(curr_idx, curr_idx + count))
+        return sum([v * i for i in indices])
+
+    total = 0
+    nums = [int(n) for n in inp]
+
+    # pointers
+    l, r = 0, len(nums) - 1
+
+    # values
+    a, b = 0, (len(nums) - 1) // 2
+
+    # one pass to get the starting index at every space and file.
+    starting_indices = [sum(nums[:i]) for i in range(len(nums))]
+
+    # We go from right to left and move every group of numbers to the left.
+
+    total = 0  # every time smth is moved, add to this total.
+    for r in range(len(nums) - 1, 0, -2):
+        v = r // 2
+        file_moved = False
+        filesize = nums[r]
+        for l in range(1, r, 2):
+            # file fits in space:
+            if nums[l] >= filesize:
+                total += tally_points(filesize, starting_indices[l], v)
+                # The empty space index is now more to the right.
+                starting_indices[l] += filesize
+                # Less empty space now.
+                nums[l] -= filesize
+                file_moved = True
+                break
+        if not file_moved:
+            total += tally_points(filesize, starting_indices[r], v)
+    return total
+
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
-        lines = [l.strip() for l in f.readlines()]
-    
-    print(solution(lines))
+        line = f.readlines()[0].strip()
+
+    print(solution(line))  # 6415163624282
