debfabd42b7ed312b2e473484296b7642f0bc325
Leon <leon.yao117@gmail.com>
2024-12-17 14:25:57 +0000

    add part 1 of day 11

---
+++ 2024/day11/part1.py
@@ -0,0 +1,23 @@
+def solution(inp):
+    curr = [int(n) for n in inp[0].split(" ")]
+    for _ in range(25):
+        next = []
+        for n in curr:
+            if n == 0:
+                next.append(1)
+            elif len(str(n)) % 2 == 0:
+                n_str = str(n)
+                next.append(int(n_str[:len(n_str) // 2]))
+                next.append(int(n_str[len(n_str) // 2:]))
+            else:
+                next.append(n * 2024)
+        
+        curr = next
+
+    return len(curr)
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        lines = [l.strip() for l in f.readlines()]
+    
+    print(solution(lines))
