f63327d52346355c11be33a506a7821d34bd2ebc
Leon <leon.yao117@gmail.com>
2024-12-18 19:43:38 +0000

    refactor part 1 code

--- 2024/day05/part1.py
+++ 2024/day05/part1.py
@@ -1,21 +1,18 @@
+from collections import defaultdict
+
+
 def solution(rules, updates):
-    hashmap = {}
+    hashmap = defaultdict(list)
     for rule in rules:
         l, r = rule.split("|")
-        if l not in hashmap:
-            hashmap[l] = [r]
-        else:
-            hashmap[l].append(r)
+        hashmap[l].append(r)
 
     updates = [u.split(",") for u in updates]
-
     total = 0
     for page_numbers in updates:
         mid = page_numbers[len(page_numbers) // 2]
         for i, num in enumerate(page_numbers):
-            if all([num in hashmap and n in hashmap[num] for n in page_numbers[i + 1 :]]):
-                continue
-            else:
+            if any([num not in hashmap or n not in hashmap[num] for n in page_numbers[i + 1 :]]):
                 mid = 0
                 break
         total += int(mid)
@@ -23,7 +20,6 @@ def solution(rules, updates):
 
 
 if __name__ == "__main__":
-    rules, nums = [], []
     idx, all_lines = -1, []
     with open("input.txt", "r") as f:
         for i, line in enumerate(f.readlines()):
