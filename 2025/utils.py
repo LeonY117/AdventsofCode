ebb712f5e2256e434686f0bffa0a43af1c0a87b3
Leon Yao <leon.yao117@gmail.com>
2025-12-08 21:44:28 +0000

    add argument to optionally strip text

--- 2025/utils.py
+++ 2025/utils.py
@@ -1,15 +1,13 @@
-def get_input_for_day(day, split_chunks=False):
+def get_input_for_day(day, split_chunks=False, strip=True):
     return get_file_for_day(
-        day,
-        filename="input",
-        split_chunks=split_chunks,
+        day, filename="input", split_chunks=split_chunks, strip=strip
     )
 
 
-def get_file_for_day(day, filename="test", split_chunks=False):
+def get_file_for_day(day, filename="test", split_chunks=False, strip=True):
     with open(f"day{day:02d}/{filename}.txt") as f:
         if not split_chunks:
-            return [l.strip() for l in f.readlines()]
+            return [l.strip() if strip else l for l in f.readlines()]
         elif split_chunks:
             all_lines = f.readlines()
             chunks, curr_chunk = [], []
@@ -18,6 +16,6 @@ def get_file_for_day(day, filename="test", split_chunks=False):
                     chunks.append(curr_chunk)
                     curr_chunk = []
                     continue
-                curr_chunk.append(line.strip())
+                curr_chunk.append(line.strip() if strip else line)
             chunks.append(curr_chunk)
             return chunks
