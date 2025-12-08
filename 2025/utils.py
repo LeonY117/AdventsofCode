b63f8a1320aaeb19755bbf7d9870ba1e5a955fc5
Leon Yao <leon.yao117@gmail.com>
2025-12-08 18:11:57 +0000

    add logic to split inputs with chunks

--- 2025/utils.py
+++ 2025/utils.py
@@ -1,10 +1,23 @@
-def get_input_for_day(day):
-    with open(f"day{day:02d}/input.txt") as f:
-        lines = [l.strip() for l in f.readlines()]
-    return lines
+def get_input_for_day(day, split_chunks=False):
+    return get_file_for_day(
+        day,
+        filename="input",
+        split_chunks=split_chunks,
+    )
 
 
-def get_file_for_day(day, filename="test"):
+def get_file_for_day(day, filename="test", split_chunks=False):
     with open(f"day{day:02d}/{filename}.txt") as f:
-        lines = [l.strip() for l in f.readlines()]
-    return lines
+        if not split_chunks:
+            return [l.strip() for l in f.readlines()]
+        elif split_chunks:
+            all_lines = f.readlines()
+            chunks, curr_chunk = [], []
+            for line in all_lines:
+                if line == "\n":
+                    chunks.append(curr_chunk)
+                    curr_chunk = []
+                    continue
+                curr_chunk.append(line.strip())
+            chunks.append(curr_chunk)
+            return chunks
