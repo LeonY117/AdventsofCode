d48141ec09efeb62754456bf2801ad451cdc69c4
Leon Yao <leon.yao117@gmail.com>
2025-12-09 13:55:40 +0000

    add day 8 solutions

---
+++ 2025/day08/part2.py
@@ -0,0 +1,68 @@
+from utils import *
+
+
+class Node:
+    def __init__(self, coord):
+        self.coord = coord
+        self.neighbors = set()
+
+    def add_neighbor(self, neighbor):
+        self.neighbors.add(neighbor)
+
+    def __str__(self):
+        return f"Node{self.coord}"
+
+
+def cartesian_distance(c1, c2):
+    return (c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2 + (c1[2] - c2[2]) ** 2
+
+
+def create_distance_to_pair_map(coords):
+    out = {}
+    for i, c1 in enumerate(coords):
+        for j, c2 in enumerate(coords):
+            if i == j:
+                continue
+            d = cartesian_distance(c1, c2)
+            if d in out and out[d][0] != c2:
+                raise ValueError("Contains duplicate distance")
+            out[d] = (c1, c2)
+    return out
+
+
+def graph_has_one_group(nodes):
+    remaining = set(nodes.keys())
+    while remaining:
+        queue = [next(iter(remaining))]
+        group_size = 0
+        while queue:
+            c = queue.pop()
+            if c not in remaining:
+                continue
+            queue += [n.coord for n in nodes[c].neighbors]
+            remaining.remove(c)
+            group_size += 1
+        if remaining:
+            return False
+    return True
+
+
+def solution(inp):
+    coords = [l.split(",") for l in inp]
+    coords = [(int(c[0]), int(c[1]), int(c[2])) for c in coords]
+    nodes = {c: Node(c) for c in coords}
+    distance_to_pair_map = create_distance_to_pair_map(coords)
+
+    # sort distance by ascending order and go through the pairs.
+    for d in sorted(distance_to_pair_map.keys()):
+        c1, c2 = distance_to_pair_map[d]
+        nodes[c1].add_neighbor(nodes[c2])
+        nodes[c2].add_neighbor(nodes[c1])
+        if graph_has_one_group(nodes):
+            return c1[0] * c2[0]
+
+
+if __name__ == "__main__":
+    lines = get_input_for_day(8)
+    # lines = get_file_for_day(8, "test_input")
+    print(solution(lines))
